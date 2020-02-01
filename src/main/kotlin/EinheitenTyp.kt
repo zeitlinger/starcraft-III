c18974d446e8c73a0cbaf2226a0bbba486403f47
Gregor Zeitlinger <gregor.zeitlinger@zalando.de>
2020-02-01 20:34:55 +0100

    mit autoverteidigung

--- src/main/kotlin/EinheitenTyp.kt
+++ src/main/kotlin/EinheitenTyp.kt
@@ -5,8 +5,6 @@ import javafx.application.Platform
 import javafx.event.EventHandler
 import javafx.scene.Scene
 import javafx.scene.control.Button
-import javafx.scene.image.Image
-import javafx.scene.image.ImageView
 import javafx.scene.input.MouseEvent
 import javafx.scene.layout.HBox
 import javafx.scene.layout.Pane
@@ -14,11 +12,11 @@ import javafx.scene.layout.VBox
 import javafx.scene.paint.Color
 import javafx.scene.shape.Arc
 import javafx.scene.shape.ArcType
+import javafx.scene.shape.Circle
 import javafx.scene.shape.Line
 import javafx.scene.text.Font
 import javafx.scene.text.Text
 import javafx.stage.Stage
-import java.io.FileInputStream
 import kotlin.math.absoluteValue
 import kotlin.math.pow
 import kotlin.math.sqrt
@@ -33,18 +31,18 @@ data class EinheitenTyp(
 )
 
 data class Einheit(
-    val reichweite: Int,
-    var leben: Int,
-    val schaden: Int,
-    var x: Double,
-    var y: Double,
-    val laufweite: Double,
-    val bild: HBox = Main.einheitenBild(),
-    val lebenText: Text = Text(),
-    var ziel: Einheit? = null,
-    var zielPunkt: Punkt? = null,
-    var zielpunktkreis: Arc? = null,
-    var zielpunktLinie: Line? = null
+        val reichweite: Int,
+        var leben: Int,
+        val schaden: Int,
+        var x: Double,
+        var y: Double,
+        val laufweite: Double,
+        val bild: Circle = Main.einheitenBild(),
+        val lebenText: Text = Text(),
+        var ziel: Einheit? = null,
+        var zielPunkt: Punkt? = null,
+        var zielpunktkreis: Arc? = null,
+        var zielpunktLinie: Line? = null
 ) {
     fun punkt() = Punkt(x = x, y = y)
 }
@@ -69,14 +67,14 @@ data class Spieler(
 @Suppress("SpellCheckingInspection")
 class Main : Application() {
 
-    val infantrie = EinheitenTyp(reichweite = 100, leben = 50, schaden = 1, laufweite = 8.0, kristalle = 50)
-    val berserker = EinheitenTyp(reichweite = 20, leben = 100, schaden = 2, laufweite = 10.0, kristalle = 80)
-    val panzer = EinheitenTyp(reichweite = 500, leben = 500, schaden = 5, laufweite = 5.0, kristalle = 250)
+    val infantrie = EinheitenTyp(reichweite = 150, leben = 1000, schaden = 1, laufweite = 0.5, kristalle = 500)
+    val berserker = EinheitenTyp(reichweite = 40, leben = 2000, schaden = 4, laufweite = 1.0, kristalle = 1000)
+    val panzer = EinheitenTyp(reichweite = 500, leben = 10000, schaden = 5, laufweite = 0.25, kristalle = 2500)
 
     val computer = Spieler(einheiten = mutableListOf(
-            einheit(x = 900.0, y = 10.0, einheitenTyp = infantrie),
-            einheit(x = 450.0, y = 10.0, einheitenTyp = berserker),
-            einheit(x = 100.0, y = 10.0, einheitenTyp = panzer)
+            einheit(x = 1000.0, y = 10.0, einheitenTyp = infantrie),
+            einheit(x = 800.0, y = 10.0, einheitenTyp = berserker),
+            einheit(x = 900.0, y = 10.0, einheitenTyp = panzer)
     ),
             kristalle = 0,
             angriffspunkte = 20,
@@ -94,11 +92,10 @@ class Main : Application() {
                     x = x,
                     y = y, laufweite = einheitenTyp.laufweite)
 
-
     val mensch = Spieler(einheiten = mutableListOf(
-            einheit(x = 900.0, y = 950.0, einheitenTyp = infantrie),
-            einheit(x = 450.0, y = 950.0, einheitenTyp = berserker),
-            einheit(x = 100.0, y = 950.0, einheitenTyp = panzer)
+            einheit(x = 1000.0, y = 950.0, einheitenTyp = infantrie),
+            einheit(x = 800.0, y = 950.0, einheitenTyp = berserker),
+            einheit(x = 900.0, y = 950.0, einheitenTyp = panzer)
     ),
             kristalle = 0,
             angriffspunkte = 20,
@@ -114,14 +111,14 @@ class Main : Application() {
 
     var ausgewaehlt: Einheit? = null
 
-    var auswahlKreis: Arc = kreis(x = -100.0, y = -100.0, radaius = 15.0)
+    var auswahlKreis: Arc = kreis(x = -100.0, y = -100.0, radius = 25.0)
 
-    private fun kreis(x: Double, y: Double, radaius: Double): Arc {
+    private fun kreis(x: Double, y: Double, radius: Double): Arc {
         return Arc().apply {
             centerX = x
             centerY = y
-            radiusX = radaius
-            radiusY = radaius
+            radiusX = radius
+            radiusY = radius
             fill = Color.TRANSPARENT
             stroke = Color.BLACK
             type = ArcType.OPEN
@@ -135,8 +132,8 @@ class Main : Application() {
         initSpieler(computer)
         initSpieler(mensch)
 
-        mensch.kristalleText.x = 1800.0
-        mensch.kristalleText.y = 30.0
+        mensch.kristalleText.x = 10.0
+        mensch.kristalleText.y = 950.0
 
         val vBox = VBox(10.0)
 
@@ -181,29 +178,35 @@ class Main : Application() {
                     runde()
                     male()
                 }
-                Thread.sleep(200)
+                Thread.sleep(15)
             }
         }).start()
     }
 
     private fun laufBefehl(einheit: Einheit, event: MouseEvent) {
-        einheit.zielPunkt = Punkt(x = event.x, y = event.y)
+        val x = event.x
+        val y = event.y
+        einheit.zielPunkt = Punkt(x = x, y = y)
         einheit.ziel = null
 
+        maleZiel(einheit, x, y)
+    }
+
+    private fun maleZiel(einheit: Einheit, x: Double, y: Double) {
         val z = einheit.zielpunktkreis
         if (z != null) {
             box.children.remove(z)
         }
         einheit.zielpunktLinie?.let { box.children.remove(it) }
 
-        einheit.zielpunktkreis = kreis(x = event.x, y = event.y, radaius = 5.0).apply {
+        einheit.zielpunktkreis = kreis(x = x, y = y, radius = 5.0).apply {
             box.children.add(this)
         }
         einheit.zielpunktLinie = Line().apply {
             startX = einheit.x
             startY = einheit.y
-            endX = event.x
-            endY = event.y
+            endX = x
+            endY = y
             box.children.add(this)
         }
     }
@@ -216,11 +219,11 @@ class Main : Application() {
     }
 
     private fun bildHinzufuegen(spieler: Spieler, einheit: Einheit) {
+        einheit.bild.fill = spieler.farbe
         box.children.add(einheit.bild)
         box.children.add(einheit.lebenText)
 
-        val imageView = einheit.bild.children.first()
-//        val imageView = einheit.bild
+        val imageView = einheit.bild
 
         if (spieler.mensch) {
             imageView.onMouseClicked = EventHandler { event ->
@@ -235,6 +238,7 @@ class Main : Application() {
                     if (einheit.leben > 0) {
                         it.ziel = einheit
                         it.zielPunkt = null
+                        maleZiel(it, einheit.x, einheit.y)
                         event.consume()
                     }
                 }
@@ -252,8 +256,8 @@ class Main : Application() {
         schiessen(computer, mensch)
         schiessen(mensch, computer)
 
-        computer.einheiten.toList().forEach { entfernen(it, computer) }
-        mensch.einheiten.toList().forEach { entfernen(it, mensch) }
+        computer.einheiten.toList().forEach { entfernen(it, computer, mensch) }
+        mensch.einheiten.toList().forEach { entfernen(it, mensch, computer) }
 
         if (computer.einheiten.isEmpty()) {
             box.children.add(Text("Sieg").apply {
@@ -263,7 +267,7 @@ class Main : Application() {
             })
         }
         if (mensch.einheiten.isEmpty()) {
-            box.children.add(Text("Game Over").apply {
+            box.children.add(Text("Niderlage").apply {
                 x = 300.0
                 y = 500.0
                 font = Font(200.0)
@@ -273,7 +277,7 @@ class Main : Application() {
 
     private fun schiessen(spieler: Spieler, gegner: Spieler) {
         spieler.einheiten.forEach {
-            val ziel = zielauswaehlen(gegner, it)
+            val ziel = zielauswaehlen(gegner, it, false)
             if (ziel != null) {
                 schiessen(it, ziel)
             }
@@ -307,10 +311,23 @@ class Main : Application() {
         val A = entfernung(einheit, ziel)
 
         val reichweite = if (zielauswaehlen(gegner, einheit) != null) einheit.reichweite else 0
-        if (A - reichweite >= 1) {
+        if (A - reichweite >= 0) {
             einheit.x += smaller(b, b * einheit.laufweite / A)
             einheit.y += smaller(a, a * einheit.laufweite / A)
         }
+
+        if (einheit.zielPunkt != null && einheit.zielPunkt == einheit.punkt()) {
+            zielEntfernen(einheit)
+        }
+    }
+
+    private fun zielEntfernen(einheit: Einheit) {
+        einheit.zielpunktLinie?.let { box.children.remove(it) }
+        einheit.zielpunktkreis?.let { box.children.remove(it) }
+        einheit.zielpunktLinie = null
+        einheit.zielpunktkreis = null
+        einheit.zielPunkt = null
+        einheit.ziel = null
     }
 
     fun smaller(a: Double, b: Double): Double {
@@ -329,23 +346,53 @@ class Main : Application() {
         }
     }
 
-    private fun zielauswaehlen(gegner: Spieler, einheit: Einheit): Einheit? {
+    private fun zielauswaehlen(gegner: Spieler, einheit: Einheit, bewegen: Boolean = true): Einheit? {
+        val NaechsteEinheit = gegner.einheiten.minBy { entfernung(einheit, it.punkt()) }
         if (gegner.mensch) {
-            return gegner.einheiten.minBy { entfernung(einheit, it.punkt()) }
+            return NaechsteEinheit
         } else {
+            if (einheit.ziel == null && NaechsteEinheit != null) {
+                if (entfernung(NaechsteEinheit, einheit.punkt()) < einheit.reichweite) {
+                    if (!bewegen) {
+                        return NaechsteEinheit
+                    }
+                }
+                //beschuetzerradius 150
+                if (bewegen) {
+                    gegner.einheiten.forEach { gEinheit ->
+                        mensch.einheiten.forEach { mEinheit ->
+
+                            if (entfernung(mEinheit, gEinheit.punkt()) < gEinheit.reichweite &&
+                                    entfernung(einheit, mEinheit.punkt()) < 150) {
+                                return gEinheit
+                            }
+                        }
+                    }
+                }
+            }
             return einheit.ziel
         }
     }
 
     private fun schiessen(einheit: Einheit, ziel: Einheit) {
-        if (entfernung(einheit, ziel.punkt()) - einheit.reichweite < 1) {
+        if (entfernung(einheit, ziel.punkt()) - einheit.reichweite < 0.0) {
             ziel.leben -= einheit.schaden
         }
     }
 
-    private fun entfernen(einheit: Einheit, spieler: Spieler) {
+    private fun entfernen(einheit: Einheit, spieler: Spieler, gegner: Spieler) {
         if (einheit.leben < 1) {
             spieler.einheiten.remove(einheit)
+            if (ausgewaehlt == einheit) {
+                ausgewaehlt = null
+                auswahlKreis.centerX = -100.0
+                auswahlKreis.centerY = -100.0
+            }
+            gegner.einheiten.forEach {
+                if (it.ziel == einheit) {
+                    it.ziel = null
+                }
+            }
             box.children.remove(einheit.bild)
             box.children.remove(einheit.lebenText)
             einheit.zielpunktLinie?.let { box.children.remove(it) }
@@ -364,7 +411,7 @@ class Main : Application() {
     fun male() {
         computer.einheiten.toList().forEach { maleEinheit(it) }
         mensch.einheiten.toList().forEach { maleEinheit(it) }
-        mensch.kristalleText.text = mensch.kristalle.toString()
+        mensch.kristalleText.text = "Kristalle: " + mensch.kristalle.toString()
 
         ausgewaehlt?.let {
             auswahlKreis.centerX = it.bild.layoutX
@@ -379,13 +426,20 @@ class Main : Application() {
         kreis.layoutY = einheit.y
 
         val lebenText = einheit.lebenText
-        lebenText.x = einheit.x
+        lebenText.x = einheit.x - 10
         lebenText.y = einheit.y - 30
         lebenText.text = einheit.leben.toString()
 
         einheit.zielpunktLinie?.apply {
             startX = einheit.x
             startY = einheit.y
+            val ziel = einheit.ziel
+            if (ziel != null) {
+                endX = ziel.x
+                endY = ziel.y
+                einheit.zielpunktkreis!!.centerX = ziel.x
+                einheit.zielpunktkreis!!.centerY = ziel.y
+            }
         }
     }
 
@@ -395,13 +449,17 @@ class Main : Application() {
         fun main(args: Array<String>) {
             launch(Main::class.java)
         }
-        fun einheitenBild(): HBox {
-            val imageView = ImageView(Image(FileInputStream("/home/gzeitlinger/Pictures/panzer_oben.png")))
-            imageView.prefWidth(50.0)
-            imageView.prefHeight(50.0)
-            imageView.isPickOnBounds = false
-            imageView.isMouseTransparent = false
-            return HBox(imageView)
+
+        fun einheitenBild(): Circle {
+            return Circle(20.toDouble())
         }
     }
 }
+//einheitenkuerzel sehen
+//Basis -> wegpunkt fuer Einheiten
+//fliegende Einheiten
+//Sanitaeter
+//Minen
+//Tech gebeude
+//K.I.
+//Upgrates
